fa34540cbbccf4bb3212976711d8412d6ec77588
mtalalned <muhammadtalalned@gmail.com>
2024-07-01 11:05:44 -0700

    percentage calculator completed

---
+++ app.js
@@ -0,0 +1,22 @@
+// Percentage Calculator
+
+// var totalMarks = 400;
+// var engMarks = Number(prompt('enter your eng marks'));
+// var mathMarks = Number(prompt('enter your math marks'));
+// var urduMarks = Number(prompt('enter your urdu marks'));
+// var islMarks = Number(prompt('enter your islamiat marks'));
+// var obtainedMarks = islMarks + urduMarks + mathMarks + engMarks;
+// var percentage = (obtainedMarks / totalMarks) * 100;
+
+// console.log ("your scored percentage is ===>", percentage + "%" )
+
+// var totalMarks = 400;
+// var engMarks = +prompt('enter your eng marks');
+// var mathMarks = +prompt('enter your math marks');
+// var urduMarks = +prompt('enter your urdu marks');
+// var islMarks = +prompt('enter your islamiat marks');
+// var obtainedMarks = islMarks + urduMarks + mathMarks + engMarks;
+// var percentage = (obtainedMarks / totalMarks) * 100;
+
+// console.log ("your scored percentage is ===>", percentage + "%" )
+
