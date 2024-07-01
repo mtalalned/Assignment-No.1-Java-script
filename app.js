7a4a26fea54e032c3496c0d9dfdecc077c411221
mtalalned <muhammadtalalned@gmail.com>
2024-07-01 13:25:30 -0700

    chapter no 1 in progress

--- app.js
+++ app.js
@@ -20,3 +20,19 @@
 
 // console.log ("your scored percentage is ===>", percentage + "%" )
 
+// Chapter No. 1
+
+// Question No. 1
+// alert('Welcome to this website')
+
+// Question No. 2
+// alert('Error ! Please enter a valid password')
+
+// Question No. 3
+// alert('Welcome to JS Land...\nHappy Coding !') 
+
+// Question No. 4
+alert('Welcome to JS Land...')
+alert('Happy Coding!')
+
+
