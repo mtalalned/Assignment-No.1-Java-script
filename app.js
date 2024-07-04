950cdb5e2eef445ca45d5d8be50eeb16176ffba0
mtalalned <muhammadtalalned@gmail.com>
2024-07-04 13:12:33 -0700

    Chapter No. 2 completed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 // var obtainedMarks = islMarks + urduMarks + mathMarks + engMarks;
 // var percentage = (obtainedMarks / totalMarks) * 100;
 
-// console.log ("your scored percentage is ===>", percentage + "%" )
+// console.log ("your scored percentage is ===>", percentage + "%" );
 
 // var totalMarks = 400;
 // var engMarks = +prompt('enter your eng marks');
@@ -18,21 +18,67 @@
 // var obtainedMarks = islMarks + urduMarks + mathMarks + engMarks;
 // var percentage = (obtainedMarks / totalMarks) * 100;
 
-// console.log ("your scored percentage is ===>", percentage + "%" )
+// console.log ("your scored percentage is ===>", percentage + "%" );
 
-// Chapter No. 1
+// CHAPTER NO. 1
 
 // Question No. 1
-// alert('Welcome to this website')
+// alert('Welcome to this website');
 
 // Question No. 2
-// alert('Error ! Please enter a valid password')
+// alert('Error ! Please enter a valid password');
 
 // Question No. 3
-// alert('Welcome to JS Land...\nHappy Coding !') 
+// alert('Welcome to JS Land...\nHappy Coding !'); 
 
 // Question No. 4
-alert('Welcome to JS Land...')
-alert('Happy Coding!')
+// alert('Welcome to JS Land...');
+// alert('Happy Coding!');
+
+// Question No. 5
+// console.log("Hello... I can run JS through my web developer\'s console");
+
+// Question No. 6 
+// Practiced in previous project
+
+// Question No. 7
+// Refer index.html for answer
 
 
+// CHAPTER NO. 2
+
+// Question No. 1
+// var username;
+
+// Question No.2
+// var myName="Muhammad Talal";
+
+// Question No.3
+// var message = 'Hello World';
+// alert (message);
+
+// Question No. 4
+// var name = "Jhone Doe";
+// var age = '15 years old';
+// var education = 'Certified mobile application development';
+
+// alert(name); alert(age); alert (education);
+
+// Question No. 5
+// var pizza = 'PIZZA\nPIZZ\nPIZ\nPI\nP';
+// alert(pizza);
+
+// Question No. 6
+// var email ="muhammadtalalned@hotmail.com";
+// alert("My email address is "+ email);
+
+// Question No. 7
+// var book = 'A smarter way to learn Javascript';
+// alert ('I am trying to learn from the Book '+ book)
+
+// Question No. 8
+// document.write ('Yah i can write html content through javascript')
+
+// Question No. 9
+// var string = '▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬'
+// alert (string)
