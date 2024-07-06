75ceb3c77d66b7cb18ceb2fe046d3017edc6e436
mtalalned <muhammadtalalned@gmail.com>
2024-07-05 23:12:58 -0700

    Chapter No. 5 completed

--- app.js
+++ app.js
@@ -106,3 +106,183 @@
 // var productNumber = +prompt("How many T-shirts do you want to order?");
 // document.write('<b>'+visitorName+'</b>'+' ordered ','<b>',productNumber,'</b>'," ",'<b>',productTitle,'</b>',' on XYZ Clothing store');
 
+// CHAPTER NO. 4
+
+// Question No. 1
+// var a = 'cat', b='dog', c='camel';
+// console.log (a,b,c);
+// var myName = 'Muhammad Talal'; var yourName = 'abid hussain'; var thirdName = 'Muhammad Hamza' 
+
+// Question No. 2
+// var myName, myName12, $_myName, $myName_, dollar23Name;
+// var $^myName, alert, 1Name, prompt, Name High;
+
+// Question No. 3
+// document.write ('<b>Rules for naming JS variables</b><br><br><br><br>Variable names can only contain numbers, $ and _, For example: $my-1st varibale<br>Variable must begin with a letter, $ or _, For example: $name, _name or name<br>Varibale names are Case sensitive<br>Variable names should not be JS Keywords');
+
+// CHAPTER NO. 5
+
+// Question No. 1
+// var a = 3, b = 5;
+// console.log ('Sum of',a,'and',b,'is',a+b);
+// document.write ('Sum of ',a,' and ',b,' is ',a+b);
+
+// Quesiton No. 2
+
+// Subtraction
+// var a = 3, b = 5;
+// console.log ('Difference of',a,'and',b,'is',a-b);
+// document.write ('Difference of ',a,' and ',b,' is ',a-b);
+
+// // Multiplication
+// var a = 3, b = 5;
+// console.log ('Multiplication of',a,'and',b,'is',a*b);
+// document.write ('Multiplication of ',a,' and ',b,' is ',a*b);
+
+// // Division
+// var a = 3, b = 5;
+// console.log ('Division of',a,'and',b,'is',a/b);
+// document.write ('Division of ',a,' and ',b,' is ',a/b);
+
+// // Modulus
+// var a = 3, b = 5;
+// console.log ('Modulus of',a,'and',b,'is',a%b);
+// document.write ('Modulus of ',a,' and ',b,' is ',a%b);
+
+// Question No. 3
+// var a;
+// document.write ('Value after variable declaration is: ??');
+// a=5;
+// document.write("<br>");
+// document.write ('Initial value: ', a);
+// a=6;
+// document.write('<br>');
+// document.write ('Value after increment is: ', a);
+// a+=7;
+// document.write('<br>');
+// document.write ('Value after addition is: ', a);
+// a-=1;
+// document.write('<br>');
+// document.write ('Value after decrement is: ', a);
+// document.write('<br>');
+// document.write ('The remainder is: ', a%3);
+
+// Question No. 4
+// var ticketCost = 600;
+// var NosofTicket = 5;
+// document.write ('Total cost to buy '+NosofTicket+' tickets to a movie is '+ticketCost*NosofTicket+'PKR');
+
+// Question No. 5
+// var tableofNumber = +prompt ('What is the number of which table is required ?');
+// document.write ('Table of '+tableofNumber);
+// document.write ('<br>');
+// document.write(tableofNumber+' X '+ 1 +' = '+tableofNumber*1);
+// document.write ('<br>');
+// document.write(tableofNumber+' X '+ 2 +' = '+tableofNumber*2);
+// document.write ('<br>');
+// document.write(tableofNumber+' X '+ 3 +' = '+tableofNumber*3);
+// document.write ('<br>');
+// document.write(tableofNumber+' X '+ 4 +' = '+tableofNumber*4);
+// document.write ('<br>');
+// document.write(tableofNumber+' X '+ 5 +' = '+tableofNumber*5);
+// document.write ('<br>');
+// document.write(tableofNumber+' X '+ 6 +' = '+tableofNumber*6);
+// document.write ('<br>');
+// document.write(tableofNumber+' X '+ 7 +' = '+tableofNumber*7);
+// document.write ('<br>');
+// document.write(tableofNumber+' X '+ 8 +' = '+tableofNumber*8);
+// document.write ('<br>');
+// document.write(tableofNumber+' X '+ 9 +' = '+tableofNumber*9);
+// document.write ('<br>');
+// document.write(tableofNumber+' X '+ 10 +' = '+tableofNumber*10);
+
+// Question No. 6
+// var celsiusTemp = 25;
+// document.write (celsiusTemp+'\u00B0'+'C'+' is '+(((celsiusTemp*9)/5)+32)+'\u00B0'+'F');
+// document.write ('<br>');
+// var farenheitTemp = 70;
+// document.write (farenheitTemp+'\u00B0'+'F'+' is '+(((farenheitTemp-32)*5)/9)+'\u00B0'+'C');
+
+// Question No. 7
+// var priceItem1 = 650, quantityItem1=3, priceItem2 = 100, quantityItem2 = 7, shippingChrages=100;
+// document.write ('<b>Shopping Cart</b>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('Price of Item 1 is '+priceItem1);
+// document.write ('<br>');
+// document.write ('Quantity of Item 1 is '+quantityItem1);
+// document.write ('<br>');
+// document.write ('Price Item 2 is '+priceItem2);
+// document.write ('<br>');
+// document.write ('Quantity of Item 2 is '+quantityItem2);
+// document.write ('<br>');
+// document.write ('Shipping Charges '+shippingChrages);
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('Total cost of your order is '+(priceItem1*quantityItem1+priceItem2*quantityItem2+shippingChrages));
+
+// // Question No. 8
+// var marksObtained=804, totalMarks=980;
+// document.write ('<b> Marks Sheet </b>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('Total marks: '+totalMarks);
+// document.write ('<br>');
+// document.write ('Marks obtained: '+marksObtained);
+// document.write ('<br>');
+// document.write ('Percentage is '+marksObtained/totalMarks*100+'%');
+
+// Question No. 9
+// document.write ('<b>Currency in PKR</b>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('Total Currency in PKR: '+(10*104.80+25*28));
+
+// Question No. 10
+// var a = 10;
+// console.log ((a+5)*10/2);
+
+// // Question No. 11
+// var currentYear = 2024, birthYear = 1996;
+// document.write ('<b>Age Calculator</b>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('Current Year: '+currentYear);
+// document.write ('<br>');
+// document.write ('Birth Year: '+birthYear);
+// document.write ('<br>');
+// document.write ('Your Age is: '+(currentYear-birthYear));
+
+// Question No. 12
+// var circleRadius = 20;
+// document.write ('<b>The Geometrizer</b>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('Radius of Circle: '+circleRadius);
+// document.write ('<br>');
+// document.write ('The circumference is: '+(2*3.142*circleRadius));
+// document.write ('<br>');
+// document.write ('The area is: '+(3.142*circleRadius*circleRadius));
+
+// Question No. 13
+// var favouriteSnack = 'chocolate chip', currentAge =15, estMaxAge=65, snacksPerDay = 3;
+// document.write ('<b>The Lifetime Supply Calculator</b>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('<br>');
+// document.write ('Favourite Snack: '+favouriteSnack);
+// document.write ('<br>');
+// document.write ('Current Age: '+currentAge);
+// document.write ('<br>');
+// document.write ('Estimated Maximum Age: '+estMaxAge);
+// document.write ('<br>');
+// document.write ('Amount of Snacks per day: '+snacksPerDay);
+// document.write ('<br>');
+// document.write ('you will need '+(snacksPerDay*(estMaxAge-currentAge))+' chocolate chip to last you until the ripe old age of '+estMaxAge);
